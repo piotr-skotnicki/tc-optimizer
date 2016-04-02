c8fff7dde173a6071c42f9f6aabe93f0f09ee9a4
piotr-skotnicki <pskotnicki@wi.zut.edu.pl>
2016-04-02 22:34:26 +0200

    Floyd-Warshall transitive closure algorithm

--- src/transitive_closure.h
+++ src/transitive_closure.h
@@ -2,7 +2,10 @@
 #define	TC_TRANSITIVE_CLOSURE_H
 
 #include <isl/map.h>
+#include <isl/union_map.h>
 
 __isl_give isl_map* tc_transitive_closure(__isl_take isl_map* R, int max);
+
+__isl_give isl_union_map* tc_floyd_warshall_transitive_closure(__isl_take isl_union_map* R);
 
 #endif // TC_TRANSITIVE_CLOSURE_H
