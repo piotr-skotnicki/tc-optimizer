dbc6d9af92e26daa0d3ce99e4fcabc1d8bcc1008
piotr-skotnicki <pskotnicki@wi.zut.edu.pl>
2019-01-12 10:56:50 +0100

    Completion script

---
+++ scripts/tc-completion.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+complete -f -W "--stencil-tiling --regular-tiling --correction-tiling --merge-tiling \
+                --lex-scheduling --sfs-tile-scheduling --sfs-single-scheduling --sfs-multiple-scheduling --free-scheduling --free-rk-scheduling --free-finite-scheduling --dynamic-free-scheduling \
+                --serial-codegen --omp-for-codegen --omp-task-codegen \
+                --isl-map-tc --isl-union-map-tc --floyd-warshall-tc --iterative-tc --tarjan-tc \
+                -b -R --report --cache -d --debug -D --version -v --help -h --inline --time --use-macros \
+                -g --out -o \
+                -m --max" \
+    tc
